c3a03fa428b6a09ef37a1aeb746fc891c4c229eb
keithy <keithy@consultant.com>
2019-05-28 20:41:39 +0100

    Added a collection of Ulid string related methods
    Ulid::fromString(  )
    Ulid::sanitizeString(  )
    Ulid::fromString( Ulid::sanitizeString() );
    Ulid::isStringAnUlid(  );

--- src/Ulid.php
+++ src/Ulid.php
@@ -15,19 +15,29 @@
 
 namespace Tuupola;
 
-class Ulid
-{
+class Ulid {
+
     const PAYLOAD_SIZE = 10;
     const PAYLOAD_ENCODED_SIZE = 16;
     const TIMESTAMP_SIZE = 6;
     const TIMESTAMP_ENCODED_SIZE = 10;
-
+    
     private $payload;
     private $timestamp;
 
-    public function __construct($timestamp = null, $payload = null)
-    {
-        $this->payload = $payload;
+    // Given that the majority of the time an Ulid is used as a string, 
+    // the current internal representation is arguably the least efficient.
+    // This approach to constuction allows future improvements to be made without
+    // changing the interface.
+    
+    public function __construct($timestamp = null, $payload = null, $alreadyEncoded = false) {
+
+        if ($alreadyEncoded) {
+            $timestamp = static::crockford()->decode($timestamp, true);
+            $payload = static::crockford()->decode($payload, false);
+        }
+
+        $this->payload = payload;
         $this->timestamp = $timestamp;
 
         if (empty($payload)) {
@@ -38,53 +48,75 @@ class Ulid
         }
     }
 
-    public static function generate()
-    {
+    public static function generate() {
         return new self;
     }
 
-    public function string()
-    {
+    public function string() {
         return $this->encodeTimeStamp() . $this->encodePayload();
     }
 
-    private function encodePayload()
-    {
-        $base32 = new Base32([
-            "characters" => Base32::CROCKFORD,
-            "padding" => false
-        ]);
+    private function encodePayload() {
 
-        $encoded = $base32->encode($this->payload);
+        $encoded = static::crockford()->encode($this->payload);
         return \str_pad($encoded, self::PAYLOAD_ENCODED_SIZE, "0", STR_PAD_LEFT);
     }
 
-    private function encodeTimeStamp()
-    {
-        $base32 = new Base32([
-            "characters" => Base32::CROCKFORD,
-            "padding" => false
-        ]);
+    private function encodeTimeStamp() {
 
-        $encoded = $base32->encode($this->timestamp);
+        $encoded = static::crockford()->encode($this->timestamp);
         return \str_pad($encoded, self::TIMESTAMP_ENCODED_SIZE, "0", STR_PAD_LEFT);
     }
 
-    public function payload()
-    {
+    public function payload() {
         return $this->payload;
     }
-    public function timestamp()
-    {
-        return $this->timestamp;
-    }
-    public function unixtime()
-    {
+
+    public function timestamp() {
         return $this->timestamp;
     }
 
-    public function __toString()
-    {
+    public function unixtime() {
+        return $this->timestamp;
+    }
+
+    public function __toString() {
         return $this->string();
+    }
+
+    //"0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+    static function crockford() {
+        static $base32;
+
+        if (!isset($base32)) {
+            $base32 = new \Tuupola\Base32([
+                "characters" => \Tuupola\Base32::CROCKFORD,
+                "padding" => false
+            ]);
+        }
+        return $base32;
+    }
+
+    static function sanitizeString(string $value) {
+        $value = str_replace(
+                ['O', 'L', 'I'],
+                ['0', '1', '1'],
+                strtoupper($value));
+
+        $value = str_pad($value, 26, "0", STR_PAD_LEFT);
+                
+        return static::isStringAnUlid($value) ? $value : false;
+    }
+
+    static function fromString(string $value) {
+        
+        $timestamp = substr($value, 0, 10);
+        $payload = substr($value, 10);
+
+        return new static($timestamp, $payload);
+    }
+
+    static function isStringAnUlid(string $value) {
+        return (1 === preg_match("/^[0-7][0-9A-HJKMNP-Z]{25}$/", $value));
     }
 }
